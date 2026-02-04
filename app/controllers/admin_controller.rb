9b05d2a931fdff35e1bf3aa106ddb2aa3b921c27
iewad <lidawei.me@gmail.com>
2026-02-05 01:07:04 +0800

    fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,10 @@
 class AdminController < ApplicationController
-  before_action :set_thread_current
+  before_action :set_thread_current, if: :authenticated?
   private
     def set_thread_current
       session[:current_campuse_id] ||= Current.user.campuses.first.id
       Thread.current[:campuse] = Current.user.campuses.find(session[:current_campuse_id])
-      session[:current_semester_id] ||= Thread.current[:campuse].school.semesters.current.first.id
+      session[:current_semester_id] ||= Thread.current[:campuse].school.semesters.current.id
       Thread.current[:semester] = Thread.current[:campuse].school.semesters.find(session[:current_semester_id])
     end
 end
