e999150db97f4da2a73f510c0c261ab141cd6004
iewad <lidawei.me@gmail.com>
2026-02-06 17:25:36 +0800

    增加插班功能

--- app/controllers/admin/klasses_controller.rb
+++ app/controllers/admin/klasses_controller.rb
@@ -43,10 +43,30 @@ class Admin::KlassesController < AdminController
       params[:klass][:students_text].split("\n").each do |line|
         line.strip!
         next if line.blank?
-        student = Thread.current[:campuse].students.where(grade_id: @klass.grade_id).find_by(name: line)
-        next if student.blank?
-
-        @klass.klass_students.find_or_create_by(student_id: student.id)
+        if line.split(" ").length == 2
+          student_name, operator_str = line.split(" ")
+          student = Thread.current[:campuse].students.where(grade_id: @klass.grade_id).find_by(name: student_name)
+          next if student.blank?
+          course_seq = operator_str[0]
+          operator = operator_str[1]
+          if operator == "+"
+            @klass.klass_students.find_or_create_by(student_id: student.id)
+            # 课程之前的考勤标记为 absent
+            course = @klass.courses.find_by(seq: course_seq)
+            next if course.blank?
+            @klass.courses.where("seq < ?", course_seq).each do |course|
+              attendance = course.attendances.find_or_create_by(student_id: student.id)
+              attendance.update!(status: "absent")
+            end
+            @klass.courses.where("seq >= ? AND start_date IS NOT NULL", course_seq).each do |course|
+              course.attendances.find_or_create_by(student_id: student.id)
+            end
+          end
+        elsif line.split(" ").length == 1
+          student = Thread.current[:campuse].students.where(grade_id: @klass.grade_id).find_by(name: line)
+          next if student.blank?
+          @klass.klass_students.find_or_create_by(student_id: student.id)
+        end
       end
       redirect_to admin_klass_path(@klass), notice: "班级更新成功"
     else
