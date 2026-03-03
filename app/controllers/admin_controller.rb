00ccaa2728e2fef76f3d5123a6b607ffa3fbaaa7
iewad <lidawei.me@gmail.com>
2026-03-03 13:46:15 +0800

    fix

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -7,7 +7,7 @@ class AdminController < ApplicationController
       Thread.current[:campuse] = Campuse.find(session[:current_campuse_id])
       session[:current_school_id] ||= Thread.current[:campuse].school.id
       Thread.current[:school] = Thread.current[:campuse].school
-      session[:current_semester_id] ||= Thread.current[:campuse].school.semesters.current.id
+      session[:current_semester_id] ||= Thread.current[:campuse].school.semesters.current(Thread.current[:school].id).id
       Thread.current[:semester] = Thread.current[:campuse].school.semesters.find(session[:current_semester_id])
     end
 end
